32d0464bd38f02fc6bf9beed5791f40d23f75f29
luucaslfs <lfs@cin.ufpe.br>
2022-01-15 20:48:25 -0300

    streaming live prices data from binance sql db

--- server_app.py
+++ server_app.py
@@ -1,31 +1,58 @@
 import pandas as pd 
-#from tdqm import tdqm
+from binance.client import Client
+import asyncio
+from binance import BinanceSocketManager, AsyncClient
+nest_asyncio.apply()
+
 from sqlalchemy import create_engine
-from binance import Client
-from binance import BinanceSocketManager
 
 client = Client()
+engine = create_engine('sqlite:///CryptoDB.db')
 
-coins = ('BTCUSDT','ETHUSDT','BNBUSDT','SOLUSDT','ADAUSDT','XRPUSDT','DOTUSDT','LUNAUSDT',
-  'DOGEUSDT','AVAXUSDT','SHIBUSDT','MATICUSDT','LTCUSDT','UNIUSDT','ALGOUSDT','TRXUSDT',
-         'LINKUSDT','MANAUSDT','ATOMUSDT','VETUSDT')
+info = client.get_exchange_info()
+symbols = [x['symbol'] for x in info['symbols']]
+exclude = ['UP','DOWN','BEAR','BULL']
+non_lev = [symbol for symbol in symbols if all(excludes not in symbol for excludes in exclude)]
+relevant = [symbol for symbol in non_lev if symbol.endswith('USDT')]
+multi = [i.lower + '@trade' for i in relevant]
 
+def createframe(msg):
+    df = pd.DataFrame([msg['data']])
+    df = df.loc[:, ['s','E','p']]
+    df.columns = ['symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    return df
 
+async def main():
+    client = await AsyncClient.create()
+    bm = BinanceSocketManager(client)
+    ms = bm.multiplex_socket(multi)
+    async with ms as tcsm:
+        while True:
+            res = await tcsm.recv()
+            if res:
+                frame = createframe(res)
+                frame.to_sql(frame.symbol[0], engine, if_exists='append', index=False)
+    
+    await client.close_connection()
 
-# bsm = BinanceSocketManager(client)
-# socket = bsm.trade_socket('BTCUSDT')
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
 
-
+coins = ('BTCUSDT','ETHUSDT','BNBUSDT','SOLUSDT','ADAUSDT')
 
 def getminutedata(symbol, lookback):
     frame = pd.DataFrame(client.get_historical_klines(symbol, '1m', lookback + ' days ago UTC'))
-    frame = frame.iloc[:,5]
+    frame = frame.iloc[:,:5]
     frame.columns = ['Time', 'Open', 'High', 'Low', 'Close']
     frame[['Open', 'High', 'Low', 'Close']] = frame[['Open', 'High', 'Low', 'Close']].astype(float)
     frame.Time = pd.to_datetime(frame.Time, unit='ms')
     return frame
 
-getminutedata('BTCUSDT', '1')
+
+
     # await socket.__aenter__()
     # msg = await socket.recv()
     # frame = createframe(msg)
