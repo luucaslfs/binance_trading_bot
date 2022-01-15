67de8c5fed5f9cbacd3f09468c6c4f13d6601be2
luucaslfs <lfs@cin.ufpe.br>
2022-01-15 19:39:20 -0300

    getminutedata() implementation

--- binance.py
+++ binance.py
@@ -1,16 +1,33 @@
 import pandas as pd 
-import sqlalchemy
+from tdqm import tdqm
+from sqlalchemy import create_engine
 from binance.client import Client
 from binance import BinanceSocketManager
 
-client = Client(api_key, api_secret)
+client = Client()
 
-bsm = BinanceSocketManager(client)
-socket = bsm.trade_socket('BTCUSDT')
+coins = ('BTCUSDT','ETHUSDT','BNBUSDT','SOLUSDT','ADAUSDT','XRPUSDT','DOTUSDT','LUNAUSDT',
+  'DOGEUSDT','AVAXUSDT','SHIBUSDT','MATICUSDT','LTCUSDT','UNIUSDT','ALGOUSDT','TRXUSDT',
+         'LINKUSDT','MANAUSDT','ATOMUSDT','VETUSDT')
 
-while True:
-    await socket.__aenter__()
-    msg = await socket.recv()
-    frame = createframe(msg)
-    frame.to_sql('BTCUSDT')
-    print(msg)
+
+
+# bsm = BinanceSocketManager(client)
+# socket = bsm.trade_socket('BTCUSDT')
+
+
+
+def getminutedata(symbol, lookback):
+    frame = pd.DataFrame(client.get_historical_klines(symbol, '1m', lookback + ' days ago UTC'))
+    frame = frame.iloc[:,5]
+    frame.columns = ['Time', 'Open', 'High', 'Low', 'Close']
+    frame[['Open', 'High', 'Low', 'Close']] = frame[['Open', 'High', 'Low', 'Close']].astype(float)
+    frame.Time = pd.to_datetime(frame.Time, unit='ms')
+    return frame
+
+getminutedata('BTCUSDT', '1')
+    # await socket.__aenter__()
+    # msg = await socket.recv()
+    # frame = createframe(msg)
+    # frame.to_sql('BTCUSDT')
+    # print(msg)
